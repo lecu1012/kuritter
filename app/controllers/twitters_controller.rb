3405eafa82855345041e57a05ad0ca51f11d3992
lecu1012 <lecu1012@gmail.com>
2017-09-29 13:39:08 +0000

    init

---
+++ app/controllers/twitters_controller.rb
@@ -0,0 +1,49 @@
+class TwittersController < ApplicationController
+  before_action :set_twitter, only: [:show, :edit, :update, :destroy]
+
+  def index
+    render layout: 'top'
+  end
+
+  def new
+    @twitter = Twitter.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    @twitter =  Twitter.new(twitter_params)
+    if @twitter.save
+      redirect_to new_twitter_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @twitter.update(twitter_params)
+      redirect_to new_twitter_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @twitter.destroy
+    redirect_to new_twitter_path
+  end
+
+
+  private
+  def twitter_params
+    params.require(:twitter).permit(:content)
+  end
+
+  def set_twitter
+    @twitter = Twitter.find(params[:id])
+  end
+end
