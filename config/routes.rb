f0db68433e0329e3601cd49bd3ebadc32b2c3bd0
lecu1012 <lecu1012@gmail.com>
2017-10-01 07:39:14 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  #get 'twitters/index'
+  get 'twitters/index'
+  #get 'sessions/new'
   resources :twitters
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
