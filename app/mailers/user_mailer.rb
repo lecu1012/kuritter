234d0755d83e43f29ea071195b9e3306fa93ccc8
lecu1012 <lecu1012@gmail.com>
2017-10-22 08:34:34 +0000

    init

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def user_mail(email)
 
-    mail(to: email, subject: "kuritterにログインしました")
+    #mail(to: email, subject: "kuritterにログインしました")
     #mail(to: "lecu1012@gmail.com", subject: "kuritterにログインしました")
   end
 end
