4a9df09abf1feeb9be17c203f76fdeaeba52c4c1
lecu1012 <lecu1012@gmail.com>
2017-10-22 07:51:35 +0000

    init

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def user_mail(contact)
     @contact = contact
 
-    mail(to: current_user.email, subject: "kuritterにログインしました")
-    #mail(to: "lecu1012@gmail.com", subject: "kuritterにログインしました")
+    #mail(to: current_user.email, subject: "kuritterにログインしました")
+    mail(to: "lecu1012@gmail.com", subject: "kuritterにログインしました")
   end
 end
